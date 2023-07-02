e76d6fcbf084f2331cfa300d9943e2faebaf7172
plaodas <agake@plao-das.com>
2023-07-02 18:21:56 +0900

    Add Styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require masonry/jquery.masonry
+//= require turbolinks
+//= require_tree .
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
